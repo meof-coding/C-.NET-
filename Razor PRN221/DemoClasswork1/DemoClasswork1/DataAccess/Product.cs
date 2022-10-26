47cedc548782e25ec1775ec2d7d895cdbbbe29e3
dev17sinka <82217333+meof-coding@users.noreply.github.com>
2022-10-26 23:50:39 +0700

    [Lab1]Fix Pagination on Search

--- Razor PRN221/DemoClasswork1/DemoClasswork1/DataAccess/Product.cs
+++ Razor PRN221/DemoClasswork1/DemoClasswork1/DataAccess/Product.cs
@@ -13,7 +13,7 @@ namespace DemoClasswork1.DataAccess
         public int ProductId { get; set; }
         public string ProductName { get; set; } = null!;
         public int? SupplierId { get; set; }
-        public int? CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public string? QuantityPerUnit { get; set; }
         public decimal? UnitPrice { get; set; }
         public short? UnitsInStock { get; set; }
--- Razor PRN221/DemoClasswork1/DemoClasswork1/Pages/Product/List.cshtml.cs
+++ Razor PRN221/DemoClasswork1/DemoClasswork1/Pages/Product/List.cshtml.cs
@@ -28,32 +28,52 @@ namespace DemoClasswork1.Pages.Product
         public PaginatedList<DataAccess.Product> ProductsPage { get; set; }
         public string CurrentFilter { get; set; }
 
+        public List<int> ListCateId { get; set; }
+
+        public int? LowestPrice { get; set; }
+
+        public int? HighestPrice { get; set; }
+
         public IList<DataAccess.Product> Product { get; set; } = default!;
         public IList<DataAccess.Category> Categories { get; set; } = default!;
 
         [BindProperty]
         public List<int> CateId { get; set; }
-        public async Task OnGetAsync(int? currentpage)
+        public async Task OnGetAsync(int? currentpage, string productName, int? lowestPrice, int? highestPrice)
         {
-            Categories = _context.Categories.ToList();
-            IQueryable<DataAccess.Product> query = GetProducts();
-            Product= query.ToList();
-            await Pagination(currentpage, query);
+            List<int> categories = new List<int>();
+            if (TempData["Categories"]!=null)
+            {
+                foreach (var item in (IEnumerable<int>)TempData["Categories"])
+                {
+                    categories.Add(item);
+                }
+            }
+           
+            await GetProducts(productName, lowestPrice, highestPrice, currentpage, categories);
         }
 
-        private IQueryable<DataAccess.Product> GetProducts()
-        {
-            IQueryable<DataAccess.Product> query = _context.Products
-                .Include(o => o.Category)
-                .Include(o => o.Supplier);
-            return query;
-        }
-
-        public async Task<IActionResult> OnPostAsync(string productName, int? lowestPrice, int? highestPrice, int? pageIndex, List<int?> CateId)
+        public async Task<IActionResult> OnPostAsync(string productName, int? lowestPrice, int? highestPrice, int? pageIndex, List<int> CateId)
         {
             CurrentFilter = productName;
+            await GetProducts(productName, lowestPrice, highestPrice, pageIndex, CateId);
+            return Page();
+        }
+
+        private async Task GetProducts(string productName, int? lowestPrice, int? highestPrice, int? pageIndex, List<int> CateId)
+        {
+            ListCateId = CateId;
+            if (ListCateId != null && ListCateId.Count!=0)
+            {
+                TempData["Categories"] = ListCateId;
+            }
+            CurrentFilter = productName;
+            LowestPrice = lowestPrice;
+            HighestPrice = highestPrice;
             Categories = _context.Categories.ToList();
-            IQueryable<DataAccess.Product> query = GetProducts();
+            IQueryable<DataAccess.Product> query = _context.Products
+               .Include(o => o.Category)
+               .Include(o => o.Supplier);
             if (!String.IsNullOrEmpty(productName))
             {
                 query = query.Where(p => p.ProductName.Contains(productName));
@@ -72,7 +92,6 @@ namespace DemoClasswork1.Pages.Product
             }
             Product = query.ToList();
             await Pagination(pageIndex, query);
-            return Page();
         }
 
         private async Task Pagination(int? pageIndex, IQueryable<DataAccess.Product> query)

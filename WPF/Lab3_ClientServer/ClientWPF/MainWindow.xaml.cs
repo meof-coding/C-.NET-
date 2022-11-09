43a283abe74ab15eddb72f72e731283690dc27e0
Thanh Tra <82217333+meofiscoding@users.noreply.github.com>
2022-11-09 11:07:45 +0700

    init Lab3

---
+++ WPF/Lab3_ClientServer/ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string server = "127.0.0.1";
+        int port = 1500;
+
+        static TcpClient client = new TcpClient("127.0.0.1", 1500);
+        static NetworkStream stream = client.GetStream();
+
+        void Reload()
+        {
+            String result = ConnectServer(server, port, "getAllProduct", null);
+            List<ProductDTO> productDTOs = JsonConvert.DeserializeObject<List<ProductDTO>>(result);
+
+            lvProducts.ItemsSource = productDTOs;
+        }
+
+        private ProductDTO getProductObject()
+        {
+            ProductDTO productDTO = new ProductDTO();
+
+            productDTO.ProductName = txtProductName.Text;
+            productDTO.SupplierId = Int32.Parse(txtSupplierID.Text);
+            productDTO.CategoryId = Int32.Parse(txtCategoryId.Text);
+            productDTO.QuantityPerUnit = txtQuantityPerUnit.Text;
+            productDTO.UnitPrice = Decimal.Parse(txtUnitPrice.Text);
+            productDTO.UnitsInStock = Int16.Parse(txtUnitsInStock.Text);
+            productDTO.UnitsOnOrder = Int16.Parse(txtUnitsOnOrder.Text);
+            productDTO.ReorderLevel = Int16.Parse(txtReorderLevel.Text);
+            productDTO.Discontinued = rbYes.IsChecked.Value ? true : false;
+
+            return productDTO;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string ConnectServer(string server, int port, string action, string parameter)
+        {
+            string path;
+            if (parameter == null)
+            {
+                path = action;
+            }
+            else
+            {
+                path = action + "/" + parameter;
+            }
+            string request, response;
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(path);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine($"Sent: {path}");
+
+            data = new Byte[102400];
+            int count = stream.Read(data, 0, data.Length);
+            response = System.Text.Encoding.ASCII.GetString(data, 0, count);
+
+            return response;
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            Reload();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            client.Close();
+
+            this.Close();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            ProductDTO productDTO = getProductObject();
+
+            String jsonProduct = JsonConvert.SerializeObject(productDTO);
+
+            String result = ConnectServer(server, port, "addProduct", jsonProduct);
+
+            Reload();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            ProductDTO productDTO = getProductObject();
+            productDTO.ProductId = Int32.Parse(txtProductId.Text);
+
+            String jsonProduct = JsonConvert.SerializeObject(productDTO);
+
+            String result = ConnectServer(server, port, "updateProduct", jsonProduct);
+
+            Reload();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            String result = ConnectServer(server, port, "deleteProduct", txtProductId.Text);
+
+            Reload();
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            //dlg.DefaultExt = ".png";
+            //dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+
+                using (StreamReader r = new StreamReader(filename))
+                {
+                    string json = r.ReadToEnd();
+                    List<ProductDTO> items = JsonConvert.DeserializeObject<List<ProductDTO>>(json);
+
+                    String productJSON = JsonConvert.SerializeObject(items);
+
+                    String output = ConnectServer(server, port, "importProduct", productJSON);
+                }
+
+                Reload();
+            }
+        }
+    }
+}
---
+++ WPF/Lab3_ClientServer/ServerConsole/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using ServerConsole.Entity;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+int count = 0;
+string host = "127.0.0.1";
+int port = 1500;
+Console.WriteLine("Server App");
+IPAddress localAddr = IPAddress.Parse(host);
+TcpListener server = new TcpListener(localAddr, port);
+server.Start();
+
+Console.WriteLine("************************");
+Console.WriteLine("waiting....");
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    Console.Write("*************************");
+    Console.WriteLine($"Number of client connected: {++count}");
+    Thread thread = new Thread(new ParameterizedThreadStart(ProcessClient));
+    thread.Start(client);
+}
+
+static void ProcessClient(object parameter)
+{
+    NorthwindContext context = new NorthwindContext();
+
+    TcpClient client = (TcpClient)parameter;
+    string data;
+    int count;
+    NetworkStream stream = client.GetStream();
+    Byte[] bytes = new Byte[1024];
+    while ((count = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+        data = System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+        string[] words = data.Split('/');
+        switch (words[0])
+        {
+            case "getById":
+                context = new NorthwindContext();
+
+                String id = words[1];
+
+                Product product = context.Products.FirstOrDefault(p => p.ProductId == Int32.Parse(id));
+                String jsonProduct = Newtonsoft.Json.JsonConvert.SerializeObject(product);
+
+                //logic request -> respone.
+                Byte[] msg1 = System.Text.Encoding.ASCII.GetBytes(jsonProduct);
+                stream.Write(msg1, 0, msg1.Length);
+                break;
+            case "getAllProduct":
+                context = new NorthwindContext();
+
+                List<Product> products = context.Products.ToList();
+                String jsonProducts = Newtonsoft.Json.JsonConvert.SerializeObject(products);
+
+                //logic request -> respone.
+                Byte[] msg2 = System.Text.Encoding.ASCII.GetBytes(jsonProducts);
+                stream.Write(msg2, 0, msg2.Length);
+                break;
+            case "addProduct":
+                context = new NorthwindContext();
+
+                string requestDataAdd = words[1];
+
+                Product addPro = JsonConvert.DeserializeObject<Product>(requestDataAdd);
+                context.Products.Add(addPro);
+                context.SaveChanges();
+
+                //logic request -> respone.
+                Byte[] msg3 = System.Text.Encoding.ASCII.GetBytes("Success");
+                stream.Write(msg3, 0, msg3.Length);
+                break;
+            case "updateProduct":
+                context = new NorthwindContext();
+
+                string requestDataUpdate = words[1];
+
+                Product updatePro = JsonConvert.DeserializeObject<Product>(requestDataUpdate);
+                context.Entry<Product>(updatePro).State = EntityState.Modified;
+                context.SaveChanges();
+
+                //logic request -> respone.
+                Byte[] msg4 = System.Text.Encoding.ASCII.GetBytes("Success");
+                stream.Write(msg4, 0, msg4.Length);
+                break;
+            case "deleteProduct":
+                context = new NorthwindContext();
+                string requestDataDelete = words[1];
+
+                int deleteId = Int32.Parse(requestDataDelete);
+
+                List<OrderDetail> orderDetails = context.OrderDetails.Where(od => od.ProductId == deleteId).ToList();
+                context.OrderDetails.RemoveRange(orderDetails);
+
+                Product deleteProduct = context.Products.FirstOrDefault(p => p.ProductId == deleteId);
+                context.Products.Remove(deleteProduct);
+
+                context.SaveChanges();
+
+                //logic request -> respone.
+                Byte[] msg5 = System.Text.Encoding.ASCII.GetBytes("Success");
+                stream.Write(msg5, 0, msg5.Length);
+                break;
+            case "importProduct":
+                context = new NorthwindContext();
+                string importJSON = words[1];
+
+                List<Product> importProducts = JsonConvert.DeserializeObject<List<Product>>(importJSON);
+
+                foreach (var p in importProducts)
+                {
+                    p.ProductId = 0;
+
+                    context.Products.Add(p);
+                    context.SaveChanges();
+                }
+
+                //logic request -> respone.
+                Byte[] msg6 = System.Text.Encoding.ASCII.GetBytes("Success");
+                stream.Write(msg6, 0, msg6.Length);
+                break;
+        }
+    }
+    client.Close();
+}
